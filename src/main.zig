7693cd288b38911312576679d3973d1f5bccc988
Johan-Mi <johanmi@protonmail.com>
2024-09-04 20:47:47 +0200

    Replace deprecated default initializer

--- src/main.zig
+++ src/main.zig
@@ -2,7 +2,7 @@ const std = @import("std");
 const Ips = @import("Ips.zig");
 
 pub fn main() !void {
-    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+    var gpa = std.heap.GeneralPurposeAllocator(.{}).init;
     defer std.debug.assert(gpa.deinit() == .ok);
     const allocator = gpa.allocator();
 
